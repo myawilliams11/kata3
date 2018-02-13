077a20ac023554083bfa0b0ddbb372e5a7d9289b
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-13 15:11:10 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -22,3 +22,31 @@ function pwr(x,n) {
 console.log(pwr(2^8))
 
 // 2^8
+
+
+function factorialize(num) {
+    return num;
+  }
+  factorialize(4);
+  console.log(factorialize(4*3*2*1))
+
+
+
+
+var fibonacci_series = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+ console.log(fibonacci_series(7));
+
+
+
