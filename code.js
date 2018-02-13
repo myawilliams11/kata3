db6459b52e33207c925f729ba3a4f813b585a60c
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-13 13:54:46 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,20 @@
+function add (number1, number2) {
+    return (number1 + number2);
+}
+console.log(add(2, 4))
+
+
+
+function multiply(6,8) {
+    var result= 0;  
+    for (let i=0; i < 8; i++)
+    result= add(result,6)
+}
+
+
+function multiply(2,8) {
+    var result= 0;  
+    for (let i=0; i < 8; i++)
+    result= add(result,6)
+}
+
