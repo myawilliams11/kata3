3b2897f3d5dfc413048225964351061657b07cca
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-13 14:41:38 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -5,16 +5,20 @@ console.log(add(2, 4))
 
 
 
-function multiply(6,8) {
+function multiply(x,y) {
     var result= 0;  
-    for (let i=0; i < 8; i++)
+    for (let i=0; i < 9; i++)
     result= add(result,6)
 }
+console.log(multiply(6*8))
+// 6*8
 
 
-function multiply(2,8) {
+function pwr(x,n) {
     var result= 0;  
-    for (let i=0; i < 8; i++)
+    for (let i=0; i < 9; i++)
     result= add(result,6)
 }
+console.log(pwr(2^8))
 
+// 2^8
